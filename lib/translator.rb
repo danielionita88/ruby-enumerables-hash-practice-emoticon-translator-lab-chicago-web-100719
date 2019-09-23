abdad7e72b4a6b3d7dcbbc35a92661cdb0d459be
Daniel I <danielionita88@yahoo.com>
2019-09-23 02:23:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,25 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+
+
+def load_library(file)
+  emoticons = YAML.load_file(file)
+  list = {}
+  emoticons.each do |meaning,emojis|
+    if !list[:get_meaning]
+      list[:get_meaning] = {emojis[1] => meaning}
+    else list[:get_meaning][emojis[1]] = meaning
+    end
+    
+    if !list[:get_emoticon]
+    list[:get_emoticon] = {emojis[0] =>emojis[1]}
+    else list[:get_emoticon][emojis[0]]=emojis[1]
+    end
+    
+  end
+  list
 end
 
 def get_japanese_emoticon
